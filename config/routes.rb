a46eebb7aa907f7905f8261f6443cc11150e50da
Jerome Caron <jerocaron@gmail.com>
2019-05-06 19:53:49 +0200

    Generate Company Group & update Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/create'
+  get 'groups/edit'
+  get 'groups/update'
+  get 'groups/destroy'
+  get 'companies/index'
+  get 'companies/show'
+  get 'companies/new'
+  get 'companies/create'
+  get 'companies/edit'
+  get 'companies/update'
+  get 'companies/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
